c97addd689271afe15e915beecafb785d7b74028
landjbs <landjbs@gmail.com>
2021-03-09 13:32:00 -0800

    prepare_recipe_dataframe

--- data/data_module.py
+++ data/data_module.py
@@ -9,12 +9,23 @@ class Recipe_Dataset(LightingDataModule):
     def __init__(self):
         pass
 
+    # preparation
     def prepare_data(self):
-        pass
+        recipe_df = self.prepare_recipe_dataframe()
+        return recipe_df
+
+    def prepare_recipe_dataframe(self):
+        recipe_df = pd.read_csv(self.PP_RECIPES_FILE)
+        recipe_df.drop([
+            'techniques', 'calorie_level', 'steps_tokens', 'name_tokens', 'i',
+            'ingredient_tokens'
+        ])
+        return recipe_df
 
     def setup(self):
         pass
 
+    # dataloaders
     def train_dataloader(self):
         pass
 
