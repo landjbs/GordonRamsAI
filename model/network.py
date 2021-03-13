453443570cd8c34e18a5b6656fe50e0638f90a0d
landjbs <landjbs@gmail.com>
2021-03-13 12:37:43 -0800

    model

--- model/network.py
+++ model/network.py
@@ -12,7 +12,17 @@ class Model(nn.Module):
             n_heads=config.n_heads
             dim_feedforward=(2*config.d_model)
         )
+        self.decoder = nn.Linear(config.d_model, config.vocab_size)
+        self.softmax = nn.Softmax(dim=(-1))
 
     def forward(self, X: torch.Tensor, M: torch.Tensor):
+        ''' '''
         E = self.embedding(X)
         E = self.transformer(E)
+        logits = self.decoder(E)
+        pi = self.softmax(logits)
+        return logits
+
+    # def init_params(self):
+        # for n, p in self.named_parameters():
+            # nn.init.xavier_uniform_(p)
