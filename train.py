42211f60476159d4c0dc50b29e3e57d3a1e25d29
landjbs <landjbs@gmail.com>
2021-03-21 18:23:43 -0700

    model init

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from data import RecipeDataModule
+from model import Model
 
 
 @hydra.main(config_name='conf/config')
@@ -10,6 +11,8 @@ def test_config(config: DictConfig):
     os.chdir(hydra.utils.get_original_cwd())
     x = RecipeDataModule(config)
     x.setup()
+    model = Model(config)
+    print(model)
 
 
 if __name__ == '__main__':
