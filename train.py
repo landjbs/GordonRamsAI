96335105155526fbe47a993e30ac9d8af5a7b1b2
landjbs <landjbs@gmail.com>
2021-03-21 18:04:13 -0700

    conf formats

--- train.py
+++ train.py
@@ -1,9 +1,19 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
 from data import RecipeDataModule
 from config import Config
 
-config = Config()
-x = RecipeDataModule(config)
-x.prepare_ingr_map()
+
+@hydra.main(config_name='conf/config')
+def test_config(config: DictConfig):
+    print(OmegaConf.to_yaml(config))
+
+test_config()
+
+# config = Config()
+# x = RecipeDataModule(config)
+# x.prepare_ingr_map()
 
 # for y in x.test_dataloader():
     # print(y)
